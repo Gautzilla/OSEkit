6b9c853cb3989943877703cabc9baa36437e22ec
Gautzilla <gauthier.berthomieu@gmail.com>
2025-05-22 12:22:15 +0200

    remove unused legacy content

--- src/OSmOSE/__init__.py
+++ src/OSmOSE/__init__.py
@@ -5,17 +5,11 @@ from pathlib import Path
 import yaml
 
 from OSmOSE import utils
-from OSmOSE.Auxiliary import Auxiliary
 from OSmOSE.config import FileName
-from OSmOSE.Dataset import Dataset
 from OSmOSE.job import Job_builder
-from OSmOSE.Spectrogram import Spectrogram
 
 __all__ = [
-    "Auxiliary",
-    "Dataset",
     "Job_builder",
-    "Spectrogram",
     "utils",
 ]
 
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,66 +3,16 @@ from __future__ import annotations
 import time
 from pathlib import Path
 
-import numpy as np
 import pandas as pd
 import pytest
-import soundfile as sf
 
 from OSmOSE.utils.core_utils import (
     file_indexes_per_batch,
     locked,
     nb_files_per_batch,
-    read_header,
-    safe_read,
 )
 from OSmOSE.utils.formatting_utils import aplose2raven
 from OSmOSE.utils.path_utils import move_tree
-
-
-@pytest.mark.unit
-@pytest.mark.filterwarnings("ignore:3 NaN detected")
-def test_safe_read(input_dir):
-    rate = 44100  # samples per second
-    duration = 3
-    rng = np.random.default_rng()
-
-    data = rng.standard_normal(duration * rate)
-    sf.write(
-        input_dir.joinpath("nonan.wav"),
-        data,
-        rate,
-        format="WAV",
-        subtype="DOUBLE",
-    )
-
-    assert np.array_equal(data, safe_read(input_dir.joinpath("nonan.wav"))[0])
-
-    nandata = data.copy()
-    expected = data.copy()
-    nandata[0], nandata[137], nandata[2055] = np.nan, np.nan, np.nan
-    expected[0], expected[137], expected[2055] = 0.0, 0.0, 0.0
-    sf.write(
-        input_dir.joinpath("nan.wav"),
-        nandata,
-        rate,
-        format="WAV",
-        subtype="DOUBLE",
-    )
-
-    assert np.array_equal(expected, safe_read(input_dir.joinpath("nan.wav"))[0])
-
-
-@pytest.mark.unit
-def test_read_header(input_dir):
-    sr = 44100
-    frames = float(sr * 3)
-    channels = 1
-    sampwidth = 4
-    size = 529272
-
-    assert (sr, frames, sampwidth, channels, size) == read_header(
-        input_dir.joinpath("test.wav"),
-    )
 
 
 @pytest.fixture
