c11400a6ee4ffa01085cb10d2cdc77d0d4134b65
Gautzilla <gauthier.berthomieu@gmail.com>
2025-06-17 16:23:49 +0200

    add ScalePart.get_indexes

--- src/OSmOSE/core_api/frequency_scale.py
+++ src/OSmOSE/core_api/frequency_scale.py
@@ -21,3 +21,12 @@ class ScalePart:
     def get_frequencies(self, nb_points: int) -> list[int]:
         """Return the frequency points of the present scale part."""
         return list(map(round, np.linspace(self.f_min, self.f_max, nb_points)))
+
+    def get_indexes(self, scale_length: int) -> tuple[int, int]:
+        """Return the indexes of the present scale part in the full scale."""
+        return int(self.p_min * scale_length), int(self.p_max * scale_length)
+
+
+class Scale:
+    def __init__(self, parts: list[ScalePart]):
+        self.parts = parts
--- tests/test_frequency_scales.py
+++ tests/test_frequency_scales.py
@@ -9,7 +9,7 @@ from OSmOSE.core_api.frequency_scale import ScalePart
         pytest.param(
             ScalePart(
                 p_min=0,
-                p_max=100,
+                p_max=1.0,
                 f_min=0,
                 f_max=3,
             ),
@@ -20,7 +20,7 @@ from OSmOSE.core_api.frequency_scale import ScalePart
         pytest.param(
             ScalePart(
                 p_min=0,
-                p_max=100,
+                p_max=1.0,
                 f_min=10,
                 f_max=12,
             ),
@@ -31,6 +31,65 @@ from OSmOSE.core_api.frequency_scale import ScalePart
     ],
 )
 def test_frequency_scale_part_get_frequencies(
-    scale_part: ScalePart, nb_points: int, expected: list[int]
+    scale_part: ScalePart,
+    nb_points: int,
+    expected: list[int],
 ) -> None:
     assert list(scale_part.get_frequencies(nb_points)) == expected
+
+
+@pytest.mark.parametrize(
+    ("scale_part", "scale_length", "expected"),
+    [
+        pytest.param(
+            ScalePart(
+                p_min=0,
+                p_max=1.0,
+                f_min=0,
+                f_max=500,
+            ),
+            10,
+            (0, 10),
+            id="full_scale",
+        ),
+        pytest.param(
+            ScalePart(
+                p_min=0,
+                p_max=0.5,
+                f_min=0,
+                f_max=500,
+            ),
+            10,
+            (0, 5),
+            id="first_half",
+        ),
+        pytest.param(
+            ScalePart(
+                p_min=0.5,
+                p_max=1.0,
+                f_min=0,
+                f_max=500,
+            ),
+            10,
+            (5, 10),
+            id="last_half",
+        ),
+        pytest.param(
+            ScalePart(
+                p_min=0.18,
+                p_max=0.38,
+                f_min=0,
+                f_max=500,
+            ),
+            10,
+            (1, 3),
+            id="float_index_to_floor",
+        ),
+    ],
+)
+def test_frequency_scale_part_get_indexes(
+    scale_part: ScalePart,
+    scale_length: int,
+    expected: list[int],
+) -> None:
+    assert scale_part.get_indexes(scale_length) == expected
